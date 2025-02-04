01cae2d59c0363d022d705afca99b9e3f6e4b594
Maricouc <clea.maricourt@gmail.com>
2025-02-04 14:29:57 +0100

    Create jacket.py

---
+++ jacket.py
@@ -0,0 +1,10 @@
+class Piles:
+  def __init__(self, diameter, description : pd.DataFrame):
+    self.description = description
+
+class LowerPart:
+  def __init__(self, description : pd.DataFrame):
+    self.description = description
+    
+class UpperPart:
+  pass
